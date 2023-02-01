be2c2d92d5e095cceb2b5f0f832fe37906c37237
Natallia Dindikova <golubeva.nathalie@gmail.com>
2023-02-01 16:09:16 +0300

    modified test with new method

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,3 +10,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
